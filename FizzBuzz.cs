5121e4773a91f7417b74d1c17a85895d4ce6fa03
sailkaz <kazimierz1sta@gmail.com>
2023-11-10 22:05:28 +0100

    Dodaj pliki projektów.

---
+++ FizzBuzz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        public static int CheckUserData()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wprowadzona wartość nie jest liczbą całkowitą\nSpróbuj jeszcze raz.");
+                    continue;
+                }
+                return userNumber;
+            }
+        }
+
+        public string CheckNumber(int userNumber)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            if (userNumber % 3 == 0 && userNumber % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (userNumber % 5 == 0)
+            {
+                return "Buzz";
+            }
+            if (userNumber % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+                return userNumber.ToString();
+        }
+    }
+    
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w grze FizzBuzz.");
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Podaj liczę całkowitą i naciśnij Enter.");
+                var userData = FizzBuzz.CheckUserData();
+                FizzBuzz fizzBuzz = new();
+
+                fizzBuzz.CheckNumber(userData);
+                Console.WriteLine(fizzBuzz.CheckNumber(userData));
+
+                var value = "y";
+
+                while (value == "y")
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Czy chcesz kontynuować? Y/N");
+                    value = Console.ReadLine();
+                    if (value?.ToLower() == "n")
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Environment.Exit(0);
+                    }
+
+                    else if (value?.ToLower() == "y")
+                        break;
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Wprowadziłeś niewłaściwą wartość {value}\nWprowadź Y aby kontynuować lub N by zakończyć");
+                        value = "y";
+                    }
+                }
+            }
+
+        }
+    }
+}
